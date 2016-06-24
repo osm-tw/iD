c2e8e71e0237d5e2e9311ae813da30bfb1b38399
Kushan Joshi <0o3ko0@gmail.com>
2016-06-24 15:11:45 +0530

    Draw rectangles

--- modules/ui/fields/lanes.js
+++ modules/ui/fields/lanes.js
@@ -1,32 +1,58 @@
 export function lanes(field, context) {
     var dispatch = d3.dispatch('change'),
+        LANE_WIDTH = 25,
         wayID,
         laneData;
 
+
     function lanes(selection) {
+        laneData = context.entity(wayID).lanes();
+        var laneCount = laneData.tagged.lanes.count || laneData.defaults.lanes.count;
+
+
         // if form field is hidden or has detached from dom, clean up.
         if (!d3.select('.inspector-wrap.inspector-hidden').empty() || !selection.node().parentNode) {
             selection.call(lanes.off);
             return;
         }
 
-        laneData = context.entity(wayID).lanes();
-
         var wrap = selection.selectAll('.preset-input-wrap')
             .data([0]);
 
-        var enter = wrap.enter()
+        wrap.enter()
             .append('div')
             .attr('class', 'preset-input-wrap');
 
-        enter
-            .append('div')
-            .attr('class', 'lane-count')
-            .append('span');
+        var surface =  wrap.selectAll('.surface')
+            .data([0]);
 
-        selection.selectAll('.lane-count')
-          .text(laneData.tagged.lanes.count || laneData.defaults.lanes.count);
+        var d = wrap.dimensions();
+        var freeSpace = d[0] - LANE_WIDTH * laneCount;
 
+        surface.enter()
+            .append('svg')
+            .attr('width', d[0])
+            .attr('height', 300)
+            .attr('class', 'surface');
+
+        var lane = surface.selectAll('.lane')
+           .data(new Array(laneCount).fill(0).map(function(l, i) {
+                return i;
+           }));
+
+        lane.enter()
+            .append('g')
+            .attr('class', 'lane')
+            .append('rect')
+            .attr('y', 75)
+            .attr('width', LANE_WIDTH)
+            .attr('height', 150);
+
+        lane.selectAll('rect').attr('x', function(d) {
+            return LANE_WIDTH*d + (freeSpace/(laneCount+1))*(d+1);
+        });
+
+        lane.exit().remove();
     }
 
 

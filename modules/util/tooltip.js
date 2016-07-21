91f0240dc02c231c0ea5aaf6535e20617e95674d
Tom MacWright <tom@macwright.org>
2016-07-21 17:43:40 -0400

    Fix tooltip lint

--- modules/util/tooltip.js
+++ modules/util/tooltip.js
@@ -6,20 +6,19 @@ export function tooltip() {
     animation = d3.functor(false),
     html = d3.functor(false),
     title = function() {
-      var title = this.getAttribute("data-original-title");
+      var title = this.getAttribute('data-original-title');
       if (title) {
         return title;
       } else {
-        title = this.getAttribute("title");
-        this.removeAttribute("title");
-        this.setAttribute("data-original-title", title);
+        title = this.getAttribute('title');
+        this.removeAttribute('title');
+        this.setAttribute('data-original-title', title);
       }
       return title;
     },
-    over = "mouseenter.tooltip",
-    out = "mouseleave.tooltip",
-    placements = "top left bottom right".split(" "),
-    placement = d3.functor("top");
+    over = 'mouseenter.tooltip',
+    out = 'mouseleave.tooltip',
+    placement = d3.functor('top');
 
   tooltip.title = function(_) {
     if (arguments.length) {
@@ -64,30 +63,30 @@ export function tooltip() {
     selection
       .on(over, null)
       .on(out, null)
-      .attr("title", function() {
-        return this.getAttribute("data-original-title") || this.getAttribute("title");
+      .attr('title', function() {
+        return this.getAttribute('data-original-title') || this.getAttribute('title');
       })
-      .attr("data-original-title", null)
-      .select(".tooltip")
+      .attr('data-original-title', null)
+      .select('.tooltip')
       .remove();
   };
 
   function setup() {
     var root = d3.select(this),
         animate = animation.apply(this, arguments),
-        tip = root.append("div")
-          .attr("class", "tooltip");
+        tip = root.append('div')
+          .attr('class', 'tooltip');
 
     if (animate) {
-      tip.classed("fade", true);
+      tip.classed('fade', true);
     }
 
     // TODO "inside" checks?
 
-    tip.append("div")
-      .attr("class", "tooltip-arrow");
-    tip.append("div")
-      .attr("class", "tooltip-inner");
+    tip.append('div')
+      .attr('class', 'tooltip-arrow');
+    tip.append('div')
+      .attr('class', 'tooltip-inner');
 
     var place = placement.apply(this, arguments);
     tip.classed(place, true);
@@ -99,40 +98,40 @@ export function tooltip() {
   function show() {
     var root = d3.select(this),
         content = title.apply(this, arguments),
-        tip = root.select(".tooltip")
-          .classed("in", true),
+        tip = root.select('.tooltip')
+          .classed('in', true),
         markup = html.apply(this, arguments),
-        innercontent = tip.select(".tooltip-inner")[markup ? "html" : "text"](content),
+        innercontent = tip.select('.tooltip-inner')[markup ? 'html' : 'text'](content),
         place = placement.apply(this, arguments),
         outer = getPosition(root.node()),
         inner = getPosition(tip.node()),
         pos;
 
     switch (place) {
-      case "top":
+      case 'top':
         pos = {x: outer.x + (outer.w - inner.w) / 2, y: outer.y - inner.h};
         break;
-      case "right":
+      case 'right':
         pos = {x: outer.x + outer.w, y: outer.y + (outer.h - inner.h) / 2};
         break;
-      case "left":
+      case 'left':
         pos = {x: outer.x - inner.w, y: outer.y + (outer.h - inner.h) / 2};
         break;
-      case "bottom":
+      case 'bottom':
         pos = {x: Math.max(0, outer.x + (outer.w - inner.w) / 2), y: outer.y + outer.h};
         break;
     }
 
     tip.style(pos ?
-      {left: ~~pos.x + "px", top: ~~pos.y + "px"} :
+      {left: ~~pos.x + 'px', top: ~~pos.y + 'px'} :
       {left: null, top: null});
 
     this.tooltipVisible = true;
   }
 
   function hide() {
-    d3.select(this).select(".tooltip")
-      .classed("in", false);
+    d3.select(this).select('.tooltip')
+      .classed('in', false);
 
     this.tooltipVisible = false;
   }
@@ -146,7 +145,7 @@ export function tooltip() {
   }
 
   return tooltip;
-};
+}
 
 function getPosition(node) {
   var mode = d3.select(node).style('position');
